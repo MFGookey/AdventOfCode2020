44ed1b424abe5448bf42c5299b4f77f8b2956d16
MFGookey <MFGookey@users.noreply.github.com>
2020-12-01 13:09:28 -0500

    Refactor the more-than-one term test to accept an IList and compare needle and result lists for equivalence

--- Day1/AdventCalculator/AdventCalculatorTests/Selector/SelectorTests.cs
+++ Day1/AdventCalculator/AdventCalculatorTests/Selector/SelectorTests.cs
@@ -65,31 +65,76 @@ namespace AdventCalculatorTests.Selector
 
     [Theory]
     [MemberData(nameof(ValidOptions))]
-    void SumFinder_TotalTermsMoreThanOneAndValidOptions_ReturnsValidOptions(IList<int> haystack, int needle, int totalTerms)
+    void SumFinder_TotalTermsMoreThanOneAndValidOptions_ReturnsValidOptions(IList<int> haystack, IList<int> needles, int totalTerms)
     {
       var sut = new sut.Selector();
-      var result = sut.SumFinder(haystack, needle, totalTerms);
+      var result = sut.SumFinder(haystack, needles.Sum(), totalTerms);
 
       Assert.Equal(totalTerms, result.Count);
-      Assert.Equal(needle, result.Sum());
+      Assert.Equal(needles.Count, result.Count);
+      Assert.Equal(needles.Sum(), result.Sum());
+      Assert.Equal(needles.OrderBy(i => i), result.OrderBy(i => i));
     }
 
     public static IEnumerable<object[]> ValidOptions {
       get
       {
-        yield return new object[] { new List<int> { 1, 2, 3 }, 4, 2 };
-        yield return new object[] { new List<int> { -1, 1, 5, 6 }, 5, 2 };
-        yield return new object[] { new List<int> { 1, 1, 1, 1, 1 }, 4, 4};
+        yield return new object[] {
+          new List<int> { 1, 2, 3 },
+          new List<int> { 1, 3 },
+          2
+        };
+
+        // Same as above, needles in reversed order
+        yield return new object[] {
+          new List<int> { 1, 2, 3 },
+          new List<int> { 3, 1 },
+          2
+        };
+
+        yield return new object[] {
+          new List<int> { -1, 1, 5, 6 },
+          new List<int> { 5, 1 },
+          2
+        };
+
+        yield return new object[] {
+          new List<int> { 1, 1, 1, 1, 1 },
+          new List<int>{ 1, 1, 1, 1 },
+          4
+        };
         
         // Provided by the Day 1 problem 1 description itself
-        yield return new object[] { new List<int> { 1721, 979, 366, 299, 675, 1456 }, 2020, 2 };
+        yield return new object[] {
+          new List<int> { 1721, 979, 366, 299, 675, 1456 },
+          new List<int> { 1721, 299 },
+          2
+        };
         
-        yield return new object[] { new List<int> { 1721, 979, 366, 299, 675, 1456 }, 665, 2 };
-        yield return new object[] { new List<int> { 1721, 979, 366, 299, -675, 1456 }, 670, 3 };
-        yield return new object[] { new List<int> { 1721, 979, 366, -299, -675, 1456 }, -974, 2 };
+        yield return new object[] {
+          new List<int> { 1721, 979, 366, 299, 675, 1456 },
+          new List<int> { 299, 366 },
+          2
+        };
+
+        yield return new object[] {
+          new List<int> { 1721, 979, 366, 299, -675, 1456 },
+          new List<int> { 979, -675, 366 },
+          3
+        };
+        
+        yield return new object[] {
+          new List<int> { 1721, 979, 366, -299, -675, 1456 },
+          new List<int> {-299, -675 },
+          2
+        };
 
         // Provided by the Day 2 problem 2 description itself
-        yield return new object[] { new List<int> { 1721, 979, 366, 299, 675, 1456 }, 2020, 3 };
+        yield return new object[] {
+          new List<int> { 1721, 979, 366, 299, 675, 1456 },
+          new List<int> { 979, 366, 675 },
+          3
+        };
       }
     }
   }
