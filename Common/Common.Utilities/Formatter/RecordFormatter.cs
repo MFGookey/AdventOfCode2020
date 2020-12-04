9878c81c5f229298106f475b594847bd12cd4782
MFGookey <MFGookey@users.noreply.github.com>
2020-12-04 10:50:56 -0500

    Add testing project to Common, add RecordFormatter with tests

---
+++ Common/Common.Utilities/Formatter/RecordFormatter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Common.Utilities.IO;
+
+namespace Common.Utilities.Formatter
+{
+  public class RecordFormatter : IRecordFormatter
+  {
+    private IFileReader _fileReader;
+
+    /// <summary>
+    /// Initializes a new instance of the RecordFormatter class with a given IFileReader
+    /// </summary>
+    /// <param name="fileReader">The file reader to use for file IO</param>
+    public RecordFormatter(IFileReader fileReader)
+    {
+      _fileReader = fileReader;
+    }
+    
+    /// <inheritdoc/>
+    public IEnumerable<string> FormatFile(string filePath, string recordDelimiter, bool removeBlankRecords)
+    {
+      var fileContent = _fileReader.ReadFile(filePath);
+      return FormatRecord(fileContent, recordDelimiter, removeBlankRecords);
+    }
+
+    /// <inheritdoc/>
+    public IEnumerable<string> FormatRecord(string records, string recordDelimiter, bool removeBlankRecords)
+    {
+      return records.Split(recordDelimiter, removeBlankRecords ? StringSplitOptions.RemoveEmptyEntries : StringSplitOptions.None).Where(r => { return removeBlankRecords ? string.IsNullOrWhiteSpace(r) == false : true; });
+    }
+
+    /// <inheritdoc/>
+    public IEnumerable<IEnumerable<string>> FormatSubRecords(IEnumerable<string> records, string subRecordDelimiter, bool removeBlankRecords)
+    {
+      return records.Where(r => { return removeBlankRecords ? string.IsNullOrWhiteSpace(r) == false : true; }).Select(r => FormatRecord(r, subRecordDelimiter, removeBlankRecords));
+    }
+  }
+}
