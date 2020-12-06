ea18ce5b13581b4ff1ea9f4a013f8f65d15d9035
MFGookey <MFGookey@users.noreply.github.com>
2020-12-05 19:41:18 -0500

    Rebasing

---
+++ Day5/NorthPoleAir/NorthPoleAir.Cmd/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Common.Utilities.IO;
+using NorthPoleAir;
+
+namespace NorthPoleAir.Cmd
+{
+  /// <summary>
+  /// The entrypoint for NorthPoleAir.Cmd
+  /// </summary>
+  public class Program
+  {
+    /// <summary>
+    /// NorthPoleAir.Cmd entry point
+    /// </summary>
+    /// <param name="args">Command line arguments (not used)</param>
+    static void Main(string[] args)
+    {
+      // I want there to be a better way to do this.
+      var filePath = "../../../../../Data/input";
+      var reader = new FileReader();
+      Console.WriteLine(reader.ReadFile(filePath).Length);
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
---
+++ Day5/NorthPoleAir/NorthPoleAir.Core/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Common.Utilities.IO;
+
+namespace NorthPoleAir.Cmd
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      var filePath = "../../../../../Data/input";
+      var reader = new FileReader();
+      Console.WriteLine(reader.ReadFile(filePath).Length);
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
