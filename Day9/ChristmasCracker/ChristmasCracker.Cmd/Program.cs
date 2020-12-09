483aded8eeeb10d2d17c8aea387d7d914a2325ba
MFGookey <MFGookey@users.noreply.github.com>
2020-12-09 15:54:03 -0500

    Initial project commit

---
+++ Day9/ChristmasCracker/ChristmasCracker.Cmd/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Common.Utilities.IO;
+//using ChristmasCracker.Core;
+
+namespace ChristmasCracker.Cmd
+{
+  /// <summary>
+  /// The entrypoint for ChristmasCracker.Cmd
+  /// </summary>
+  public class Program
+  {
+    /// <summary>
+    /// ChristmasCracker.Cmd entry point
+    /// </summary>
+    /// <param name="args">Command line arguments (not used)</param>
+    static void Main(string[] args)
+    {
+      var filePath = "./input";
+      var reader = new FileReader();
+      Console.WriteLine(reader.ReadFile(filePath).Length);
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
