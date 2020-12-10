51de865eeb7593ef293d05054a9ae3315f58d4d0
MFGookey <MFGookey@users.noreply.github.com>
2020-12-09 23:29:47 -0500

    Convert ISelector to be primarily working with long

--- Common/Common.Utilities/Selector/ISelector.cs
+++ Common/Common.Utilities/Selector/ISelector.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Common.Utilities.Selector
 {
@@ -7,6 +8,15 @@ namespace Common.Utilities.Selector
   /// </summary>
   public interface ISelector
   {
+    /// <summary>
+    /// Given a list of ints, a target sum, and the number of terms to include in the final list, return an enumerable of those ints
+    /// </summary>
+    /// <param name="haystack">The list of ints to search for a sum equalling the target value</param>
+    /// <param name="needle">The target value to which the returned list must sum</param>
+    /// <param name="totalTerms">The number of ints that ought to be in the final listing</param>
+    /// <returns>An enumerable of integers that sum to a target value</returns>
+    IEnumerable<long> SumFinder(IEnumerable<long> haystack, long needle, int totalTerms);
+  
     /// <summary>
     /// Given a list of ints, a target sum, and the number of terms to include in the final list, return an enumerable of those ints
     /// </summary>
